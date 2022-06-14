949d056d333688e73b22940b34cf4ed4e10604f2
Melis <tyn.melis.ru@gmail.com>
2022-06-14 18:27:30 +0300

    add loader for team

--- pages/team/futsal/mainTeam/index.js
+++ pages/team/futsal/mainTeam/index.js
@@ -2,10 +2,12 @@ import styles from "../../../../styles/team/futsal.module.sass";
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
 import axios from "../../../../api/axios.news";
+import Error from "../../../../components/Error/Error";
+import Loader from "../../../../components/Loader/Loader";
 
 export default function mainTeam() {
-  const [firstData, setFirstData] = useState([]);
-  const [secondData, setSecondData] = useState([]);
+  const [firstData, setFirstData] = useState(null);
+  const [secondData, setSecondData] = useState(null);
   const [checkedRadio, setCheckedRadio] = useState(true);
   useEffect(() => {
     const getFirstData = async () => {
@@ -15,7 +17,9 @@ export default function mainTeam() {
           throw new Error();
         }
         setFirstData(res.data.data);
-      } catch (error) {}
+      } catch (error) {
+        setFirstData("Error");
+      }
     };
     getFirstData();
     const getSecondData = async () => {
@@ -25,7 +29,9 @@ export default function mainTeam() {
           throw new Error();
         }
         setSecondData(res.data.data);
-      } catch (error) {}
+      } catch (error) {
+        setSecondData("Error");
+      }
     };
     getSecondData();
   }, []);
@@ -37,78 +43,89 @@ export default function mainTeam() {
       setCheckedRadio(false);
     }
   };
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Национальная сборная Кыргызской Республики по футзалу</title>
-        <meta
-          name="description"
-          content="Национальная сборная Кыргызской Республики по футзалу"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1 className={styles.main__title}>
-        Национальная сборная Кыргызской Республики по футзалу
-      </h1>
-      <div className={styles.team}>
-        <form className={styles.team__radioBtn}>
-          <span className={styles.teamCheckbox__span}>
-            <input
-              onChange={changeHandler}
-              type="radio"
-              name="team"
-              id="team__1"
-              checked={checkedRadio}
-              value={"first"}
+  if (firstData && secondData) {
+    if (firstData === "Error" && secondData === "Error") {
+      return <Error />;
+    } else {
+      return (
+        <div className={styles.container}>
+          <Head>
+            <title>Национальная сборная Кыргызской Республики по футзалу</title>
+            <meta
+              name="description"
+              content="Национальная сборная Кыргызской Республики по футзалу"
             />
-            <label htmlFor="team__1">Тренерский штаб</label>
-          </span>
-          <span className={styles.teamCheckbox__span}>
-            <input
-              onChange={changeHandler}
-              type="radio"
-              name="team"
-              id="team__2"
-              value={"second"}
-            />
-            <label htmlFor="team__2">Состав команды</label>
-          </span>
-        </form>
-        <ul className={styles.players__list}>
-          {checkedRadio
-            ? firstData.map(({ name, image, id }) => {
-                return (
-                  <li key={id} className={styles.playersList__item}>
-                    <div className={styles.personImage}>
-                      {image ? (
-                        <img src={image.url} alt={image.name} />
-                      ) : (
-                        <img
-                          src="/images/team/mainNoname.png"
-                          alt="footballUser"
-                        />
-                      )}
-                    </div>
-                    <span className={styles.playersItem__text}>{name}</span>
-                  </li>
-                );
-              })
-            : secondData.map(({ name, image, id }) => {
-                return (
-                  <li key={id} className={styles.playersList__item}>
-                    <div className={styles.personImage}>
-                      {image ? (
-                        <img src={image.url} alt={image.name} />
-                      ) : (
-                        <img src="/images/team/noname.png" alt="footballUser" />
-                      )}
-                    </div>
-                    <span className={styles.playersItem__text}>{name}</span>
-                  </li>
-                );
-              })}
-        </ul>
-      </div>
-    </div>
-  );
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <h1 className={styles.main__title}>
+            Национальная сборная Кыргызской Республики по футзалу
+          </h1>
+          <div className={styles.team}>
+            <form className={styles.team__radioBtn}>
+              <span className={styles.teamCheckbox__span}>
+                <input
+                  onChange={changeHandler}
+                  type="radio"
+                  name="team"
+                  id="team__1"
+                  checked={checkedRadio}
+                  value={"first"}
+                />
+                <label htmlFor="team__1">Тренерский штаб</label>
+              </span>
+              <span className={styles.teamCheckbox__span}>
+                <input
+                  onChange={changeHandler}
+                  type="radio"
+                  name="team"
+                  id="team__2"
+                  value={"second"}
+                />
+                <label htmlFor="team__2">Состав команды</label>
+              </span>
+            </form>
+            <ul className={styles.players__list}>
+              {checkedRadio
+                ? firstData.map(({ name, image, id }) => {
+                    return (
+                      <li key={id} className={styles.playersList__item}>
+                        <div className={styles.personImage}>
+                          {image ? (
+                            <img src={image.url} alt={image.name} />
+                          ) : (
+                            <img
+                              src="/images/team/mainNoname.png"
+                              alt="footballUser"
+                            />
+                          )}
+                        </div>
+                        <span className={styles.playersItem__text}>{name}</span>
+                      </li>
+                    );
+                  })
+                : secondData.map(({ name, image, id }) => {
+                    return (
+                      <li key={id} className={styles.playersList__item}>
+                        <div className={styles.personImage}>
+                          {image ? (
+                            <img src={image.url} alt={image.name} />
+                          ) : (
+                            <img
+                              src="/images/team/noname.png"
+                              alt="footballUser"
+                            />
+                          )}
+                        </div>
+                        <span className={styles.playersItem__text}>{name}</span>
+                      </li>
+                    );
+                  })}
+            </ul>
+          </div>
+        </div>
+      );
+    }
+  } else {
+    return <Loader />;
+  }
 }
