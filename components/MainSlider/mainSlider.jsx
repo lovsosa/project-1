257a22f92f38562e1346f981ebdf79d32e9d9cda
Melis <tyn.melis.ru@gmail.com>
2022-06-16 11:48:14 +0300

    change folder name

--- components/MainSlider/mainSlider.jsx
+++ components/MainSlider/mainSlider.jsx
@@ -7,7 +7,6 @@ import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/bundle";
 import styles from "../../styles/Home.module.sass";
-
 export default function MainSlider() {
   return (
     <Swiper
